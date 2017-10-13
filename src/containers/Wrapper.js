47be8647e17072c1332e75f24d5f260f924841b9
Matt Bilbow <hi@mattbilbow.co.uk>
2017-10-13 17:08:56 +0100

    User Provider

---
+++ src/containers/Wrapper.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Wrapper extends Component {
+  render() {
+    return (
+      <div className="gridster-wrapper">
+        <img src="../assets/images/gridster-logo.png" className="gridster-logo"/>
+        render the grid here later
+      </div>
+    );
+  }
+}
+
+export default Wrapper;
--- src/main.js
+++ src/main.js
@@ -1,26 +1,14 @@
-'use strict';
-
 require('./main.scss');
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MainComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      athing: 'itsstate',
-    };
-  }
+import { Provider } from 'react-redux';
+import { Wrapper } from './containers';
 
-  render() {
-    return (
-      <div className="gridster-wrapper">
-        <img src="../assets/images/gridster-logo.png" className="gridster-logo"/>
-        render the grid here later
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<MainComponent />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider>
+    <Wrapper />
+  </Provider>,
+  document.getElementById('app')
+)
